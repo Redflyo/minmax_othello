2cbc644db8579d99dbbc493d3a380f2924928fd7
C-0-S-M-0-S <85568120+C-0-S-M-0-S@users.noreply.github.com>
2023-04-15 13:59:34 +0200

    Constants, GUI call in main script, game loop

--- othello.py
+++ othello.py
@@ -1,8 +1,16 @@
 from minmax import AI, State
 from copy import deepcopy
-size_tab = 8
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import Ui_Othello
+import sys
+from time import sleep
+
+SIZE_TAB = 8
+NB_COUPS = 5
+
 def init_tab():
-    tab = [[None for i in range(size_tab)] for j in range(size_tab)]
+    tab = [[None for i in range(SIZE_TAB)] for j in range(SIZE_TAB)]
     (tab[3])[3] = 1
     (tab[4])[4] = 1
     (tab[3])[4] = 2
@@ -20,8 +28,8 @@ def get_score(player):
 def utility_funct(state):
     score = 0
     border_memory = [[[False,False],[False,False]],[[False,False],[False,False]]]
-    for i in range(size_tab):
-        for j in range(size_tab):
+    for i in range(SIZE_TAB):
+        for j in range(SIZE_TAB):
             cell_value = state.get_cell(i,j)
             if cell_value is not None:
                 local_score = 0
@@ -33,19 +41,22 @@ def utility_funct(state):
                         border_memory[player_index][0][0] = True
                     else:
                         local_score += cell_score * 2
+
                 if j == 0:
                     if not border_memory[player_index][0][1]:
                         local_score += cell_score * 5
                         border_memory[player_index][0][1] = True
                     else:
                         local_score += cell_score * 2
-                if i == size_tab-1:
+
+                if i == SIZE_TAB-1:
                     if not border_memory[player_index][1][0]:
                         local_score += cell_score * 5
                         border_memory[player_index][1][0] = True
                     else:
                         local_score += cell_score * 2
-                if j == size_tab-1:
+
+                if j == SIZE_TAB-1:
                     if not border_memory[player_index][1][1]:
                         local_score += cell_score * 5
                         border_memory[player_index][1][1] = True
@@ -54,10 +65,7 @@ def utility_funct(state):
 
                 if local_score == 0:
                     local_score += cell_score
-
-                score += local_score
-                
-
+                score += local_score          
     return score
             
 
@@ -68,7 +76,7 @@ def get_direction_possible():
 def case_can_be_play(coordinate,state,player):
     x,y = coordinate
     direction_possible = get_direction_possible()
-    for i in range(1,size_tab):        
+    for i in range(1,SIZE_TAB):        
         for d in direction_possible:
             if d[2]:
                 cell_target = state.get_cell(x+i*d[0], y+i*d[1])
@@ -83,13 +91,13 @@ def add_token(state,x,y,player):
     for d in get_direction_possible():
         case_to_change_in_this_direction = []
         i=1
-        while i < size_tab and d[2]:
+        while i < SIZE_TAB and d[2]:
             cell_target = state.get_cell(x+i*d[0], y+i*d[1])   
             if cell_target not in (player,None):        
                 case_to_change_in_this_direction.append((x+i*d[0],y+i*d[1]))
             else:
                 if cell_target == player and i > 1:
-                    i = size_tab
+                    i = SIZE_TAB
                 else:
                     d[2] = False
             i+=1
@@ -103,33 +111,78 @@ def add_token(state,x,y,player):
     for c in case_to_change:
         state.set_cell(c[0],c[1],player)
     
-
+    state.set_cell(x,y,player)
     return state
 
 
 def action_funct(state,player):
 
-    all_none_case = [(x,y) for x in range(size_tab) for y in range(size_tab) if state.get_cell(x,y) is None]
+    all_none_case = [(x,y) for x in range(SIZE_TAB) for y in range(SIZE_TAB) if state.get_cell(x,y) is None]
 
     for c in all_none_case:
         if case_can_be_play(c,state,player):
             new_state = deepcopy(state)
             new_state = add_token(new_state,c[0],c[1],player)
 
-            new_state.set_cell(c[0],c[1],player)
+            # new_state.set_cell(c[0],c[1],player)
             yield new_state
+    # yield state
 
 def end_funct(state):
     new_state = next(action_funct(state,1))
     if new_state is None:
         return next(action_funct(state,2)) is None
+    else:
+        return False
 
 # Main
-# init
-state = State(init_tab)
-ia = AI()
+if __name__ == "__main__":
+    # init
+    state = State(init_tab)
+    ia = AI()
 
-# début de partie
-# 1er coups des noirs, noir = min
-v,next_move = ia.min_value(5,state,utility_funct,action_funct,end_funct)
-print(next_move)
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    gui = Ui_Othello.Ui_MainWindow()
+    gui.setupUi(MainWindow)
+    MainWindow.show()
+
+    # début de partie
+    # print("state.tab :\n", state.tab)
+    gui.refresh_grille(state)
+    # 1er coups des noirs, noir = min
+    if gui.couleur is gui.tour:
+        gui.label_infos.setText("Humain joue les Noirs et commence !")
+    else :
+        gui.label_infos.setText("IA joue les Noirs et commence !")
+
+    while not end_funct(state):
+        print(state)
+        if gui.couleur is gui.tour:
+            # il s'agit du tour du joueur humain
+            print('joueur')
+            # print(state)
+            while not gui.clic_joueur :
+                QtCore.QCoreApplication.processEvents()
+            gui.clic_joueur = False
+            # print("done")
+            new_state = add_token(state,gui.clicked_cells[-1][0],gui.clicked_cells[-1][1], 1 if gui.couleur else 2)
+            # print(new_state)
+
+        elif not gui.couleur:
+            print("IA Noir")
+            v,new_state = ia.min_value(NB_COUPS,state,utility_funct,action_funct,end_funct)
+
+        else :
+            print("IA Blanc")
+            v,new_state = ia.max_value(NB_COUPS,state,utility_funct,action_funct,end_funct)
+
+        state = new_state
+        # inversion de qui joue le prochain coup
+        gui.tour = not gui.tour
+        print("refresh")
+        gui.refresh_grille(state)
+        gui.label_infos.setText("Noir joue !" if  gui.couleur and gui.tour else "Blanc joue !")
+
+
+    sys.exit(app.exec_())
